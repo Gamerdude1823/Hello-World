0bab03507dbf3be46731743e608ed89c0b3fff7c
Zack Williams <wil18020@byui.edu>
2023-09-16 20:06:27 -0600

    Finish Hello World

--- hello.world.py
+++ hello.world.py
@@ -1,1 +1,21 @@
-print("Hello World! Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
+import time
+
+print("Hello World! Did you ever hear the tragedy of Darth Plagueis The Wise?")
+time.sleep(1.0) #wait one second
+print("I thought not. It is not a story the Jedi would tell you.")
+time.sleep(1.0) #wait one second
+print("It is a Sith legend.")
+time.sleep(1.0) #wait one second
+print("Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life…")
+time.sleep(1.0) #wait one second
+print("He had such a knowledge of the dark side that he could even keep the ones he cared about from dying.") 
+time.sleep(1.0) #wait one second
+print("The dark side of the Force is a pathway to many abilities some consider to be unnatural.")
+time.sleep(1.0) #wait one second
+print("He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did.")
+time.sleep(1.0) #wait one second
+print("Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep.")
+time.sleep(1.0) #wait one second
+print("Ironic.")
+time.sleep(1.0) #wait one second
+print("He could save others from death, but not himself.")
